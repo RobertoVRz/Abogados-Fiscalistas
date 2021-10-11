025e9ace023958e0a9ae11649abfded33e0d1327
RobertoVrz <roberto.vzrz@gmail.com>
2021-10-11 01:50:36 -0500

    Mapa rreglado

--- components/Contacto.vue
+++ components/Contacto.vue
@@ -128,9 +128,6 @@
     </div>
     <div
       class="contacto__mapa"
-      data-aos="fade-left"
-      data-aos-delay="500"
-      data-aos-anchor=".contacto"
     >
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3774.7822515180746!2d-96.94761778460608!3d18.89673878719098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85c4e514641e669b%3A0x2705e26bbf9fd212!2sAv%2011%202001-Int%20301%2C%20San%20Jose%2C%2094560%20C%C3%B3rdoba%2C%20Ver.!5e0!3m2!1ses!2smx!4v1629357860551!5m2!1ses!2smx"
@@ -186,7 +183,7 @@ export default {
     }
 
     @include screen(desktop) {
-      width: 50vw;
+      width: 49vw;
       height: 100vh;
     }
   }
